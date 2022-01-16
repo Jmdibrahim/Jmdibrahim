e41283ad05927831ad5ab47e2e0291b201d62695
Jmdibrahim <95353812+Jmdibrahim@users.noreply.github.com>
2022-01-16 21:22:38 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,104 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="css/aboutpage.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+</head>
+<body>
+ <button class="homebtn" ><i class="fa fa-bars" aria-hidden="true"></i><a href="index.php">HOME</a></button>
+<div class="about-section">
+
+  <h1>About Us </h1>
+  <p>The purpose of creating this app is that there is no separate blood bank app for Tirunelveli district.
+</p>
+  <p>and we hope that people  will benefit  from this</p>
+</div>
+
+<h3 style="text-align:center">OUR TEAM</h3>
+<div  class="roow">
+  <div class="kaalam">
+    <div class="kaard">
+      <img src="ras.jpg" alt="Rasik" style="width:100%">
+      <div class="kantainer">
+        <h2>Abdul Rasik</h2>
+        <p class="taitile"> Developer</p>
+       
+        <p><a href="mailto: abdulrasik345@gmail.com">abdulrasik345@gmail.com</a></p><br>
+        <div class="desc"><b><i class="fa fa-phone-square" aria-hidden="true"></i><a href="tel:+918220535902" class="desc">  CALL<a></b></div>
+       
+      </div>
+    </div>
+  </div>
+  <br>
+
+  <div class="kaalam">
+    <div class="kaard">
+      <img src="user.png" alt="Faiz" style="width:100%">
+      <div class="kantainer">
+       <h2>Faizal Mohamed</h2>
+               <p class="taitile"> Developer</p>
+       <p> <a href = "mailto: nellaibloodbank72gmail.com">nellaibloodbank72gmail.com</a></p><br>
+        <div class="desc"><b><i class="fa fa-phone-square" aria-hidden="true"></i><a href="tel:+917092492761"  class="desc">  CALL<a></b></div>
+      </div>
+    </div>
+  </div>
+ <br><br><br><br><br><br><br>
+ 
+  
+
+
+
+<script type="text/javascript">
+    (function () {
+        var options = {
+            whatsapp: "+917092492761", // WhatsApp number
+            call_to_action: "Message us", // Call to action
+            position: "left", // Position may be 'right' or 'left'
+            pre_filled_message: "welcome to Our site", // WhatsApp pre-filled message
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+</script>
+
+<!--Bootstrap -->
+<footer>
+
+
+<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+<br><br>
+<div class="connect">
+	 <p>Connect with Us:</p>
+            <ul style="list-style-type:circle">
+    
+              <li><a href="http://www.facebook.com"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+              <li><a href="http://www.twitter.com"><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
+              <li><a href="http://www.linkedin.com"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
+              <li><a href="http://www.googleplus.com"><i class="fa fa-google-plus-square" aria-hidden="true"></i></a></li>
+               
+            </ul>
+            </div>
+            	
+<footer class="footer text-center bg-light">
+	 
+	 ©  <span id="demo"></span>.   All Rights Reserved . |<span class="brand"> Desing by   <a href="http://www.google.com"> AL-QIYAMAH</a> </span><br><br> 
+	<script>
+		date = new Date();
+    	document.getElementById("demo").innerHTML=date.getFullYear();
+    </script>
+</footer>
+
+
+</body>
+
+</html>
+
+
+
+
+
